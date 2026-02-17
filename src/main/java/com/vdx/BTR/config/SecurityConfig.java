ef388dfe1850f912cedac2dc74244764a79f0848
vdx <michwoda@gmail.com>
2026-02-17 15:42:42 +0100

    Project setup, creating entities and repositories, adding basic registration&login.

--- src/main/java/com/vdx/BTR/config/SecurityConfig.java
+++ src/main/java/com/vdx/BTR/config/SecurityConfig.java
@@ -39,7 +39,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/h2-console/**").permitAll()
                         .requestMatchers("/register").permitAll()
-                        .requestMatchers("/login").permitAll()   // <<< WAŻNE
+                        .requestMatchers("/login").permitAll()
                         .anyRequest().authenticated()
                 )
                 .formLogin(form -> form
